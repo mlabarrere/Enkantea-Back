6bb80de1b33dca41cb816624a33bde3a4da13cf2
Mickaël Labarrère <35226465+mlabarrere@users.noreply.github.com>
2024-06-25 16:40:02 +0200

    Add files via upload

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,31 +1,21 @@
 import pytest
-from pytest_postgresql import factories
-from sqlmodel import SQLModel, create_engine, Session
+from app.core.config import settings
+from app.core.database import init_db, get_db_session
 
-# Import everything
-from app.models.lots import Lot  # noqa: F401
-from app.models.users import User  # noqa: F401
-from app.models.clients import Client  # noqa: F401
-from app.models.organisations import Organisation  # noqa: F401
-from app.models.sales import Sale  # noqa: F401
-from app.models.invoices import Invoice  # noqa: F401
-from app.models.sellers import Seller  # noqa: F401
+@pytest.fixture(scope="session", autouse=True)
+def set_test_settings():
+    settings.ENVIRONMENT = "tests"
 
-
-def load_database(**kwargs):
-    connection_string = f"postgresql+psycopg2://{kwargs['user']}:@{kwargs['host']}:{kwargs['port']}/{kwargs['dbname']}"
-    engine = create_engine(connection_string)
-    SQLModel.metadata.create_all(engine)
-
-
-postgresql_proc = factories.postgresql_proc(load=[load_database])
-postgresql = factories.postgresql("postgresql_proc")
-
+@pytest.fixture(autouse=True)
+def setup_test_db():
+    init_db()  # Initialise la base de données pour les tests
+    yield
+    # Nettoyage après chaque test
+    with get_db_session() as session:
+        session.close()
+    init_db()  # Réinitialise la base de données pour le prochain test
 
 @pytest.fixture
-def db_session(postgresql):
-    connection_string = f"postgresql+psycopg2://{postgresql.info.user}:@{postgresql.info.host}:{postgresql.info.port}/{postgresql.info.dbname}"
-    engine = create_engine(connection_string)
-
-    with Session(engine) as session:
+def db_session():
+    with get_db_session() as session:
         yield session
