3a8990abbf37e6bdcdae691ebb71e9bff9ac0864
Mickaël Labarrère <mickael.labarrere@gmail.com>
2024-06-25 16:24:26 +0200

    Create exceptions.py

---
+++ app/core/exceptions.py
@@ -0,0 +1,54 @@
+class DatabaseOperationError(Exception):
+    """Exception raised for errors in database operations."""
+
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+
+class LotNotFoundError(Exception):
+    """Exception raised when a lot is not found."""
+
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+
+class OrganisationNotFoundError(Exception):
+    """Exception raised when an organisation is not found."""
+
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+
+class UserNotFoundError(Exception):
+    """Exception raised when a user is not found."""
+
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+
+class ClientNotFoundError(Exception):
+    """Exception raised when a client is not found."""
+
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+
+class SaleNotFoundError(Exception):
+    """Exception raised when a client is not found."""
+
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+
+class SellerNotFoundError(Exception):
+    """Exception raised when a client is not found."""
+
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
