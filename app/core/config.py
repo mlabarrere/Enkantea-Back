ac355d842ac3ca2a1ce89f2cbbf5df7a397f5792
Mickaël Labarrère <mickael.labarrere@gmail.com>
2024-06-25 16:24:11 +0200

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,18 +1,7 @@
 import secrets
-import warnings
 from typing import Annotated, Any, Literal
-
-from pydantic import (
-    AnyUrl,
-    BeforeValidator,
-    HttpUrl,
-    PostgresDsn,
-    computed_field,
-    model_validator,
-)
-from pydantic_core import MultiHostUrl
+from pydantic import (AnyUrl,BeforeValidator,computed_field)
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from typing_extensions import Self
 
 
 def parse_cors(v: Any) -> list[str] | str:
@@ -32,7 +21,22 @@ class Settings(BaseSettings):
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     DOMAIN: str = "localhost"
-    ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+    ENVIRONMENT: Literal["local", "tests", "staging", "production"] = "local"
+
+    @property
+    def DATABASE_URL(self) -> str:
+        match self.ENVIRONMENT:
+            case "local":
+                return "sqlite:///database.db"
+            case "tests":
+                return "sqlite:///database.db"#self._database_url
+            case "staging":
+                return "postgresql://postgres:postgres@localhost:5432/postgres"
+            case "production":
+                return "postgresql://postgres:postgres@localhost:5432/postgres"
+            case _:
+                raise ValueError(f"Invalid environment: {self.ENVIRONMENT}")
+
 
     @computed_field  # type: ignore[misc]
     @property
@@ -45,77 +49,6 @@ class Settings(BaseSettings):
     BACKEND_CORS_ORIGINS: Annotated[list[AnyUrl] | str, BeforeValidator(parse_cors)] = (
         []
     )
-
-    PROJECT_NAME: str = "Enkantea"
-    SENTRY_DSN: HttpUrl | None = None
-    POSTGRES_SERVER: str = "Enkantea"
-    POSTGRES_PORT: int = 5432
-    POSTGRES_USER: str = "Enkantea"
-    POSTGRES_PASSWORD: str = "Enkantea"
-    POSTGRES_DB: str = ""
-
-    @computed_field  # type: ignore[misc]
-    @property
-    def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
-        return MultiHostUrl.build(
-            scheme="postgresql+psycopg",
-            username=self.POSTGRES_USER,
-            password=self.POSTGRES_PASSWORD,
-            host=self.POSTGRES_SERVER,
-            port=self.POSTGRES_PORT,
-            path=self.POSTGRES_DB,
-        )
-
-    SMTP_TLS: bool = True
-    SMTP_SSL: bool = False
-    SMTP_PORT: int = 587
-    SMTP_HOST: str | None = None
-    SMTP_USER: str | None = None
-    SMTP_PASSWORD: str | None = None
-    # TODO: update type to EmailStr when sqlmodel supports it
-    EMAILS_FROM_EMAIL: str | None = None
-    EMAILS_FROM_NAME: str | None = None
-
-    @model_validator(mode="after")
-    def _set_default_emails_from(self) -> Self:
-        if not self.EMAILS_FROM_NAME:
-            self.EMAILS_FROM_NAME = self.PROJECT_NAME
-        return self
-
-    EMAIL_RESET_TOKEN_EXPIRE_HOURS: int = 48
-
-    @computed_field  # type: ignore[misc]
-    @property
-    def emails_enabled(self) -> bool:
-        return bool(self.SMTP_HOST and self.EMAILS_FROM_EMAIL)
-
-    # TODO: update type to EmailStr when sqlmodel supports it
-    EMAIL_TEST_USER: str = "test@example.com"
-    # TODO: update type to EmailStr when sqlmodel supports it
-    FIRST_SUPERUSER: str = "Enkantea"
-    FIRST_SUPERUSER_PASSWORD: str = "Enkantea"
-    USERS_OPEN_REGISTRATION: bool = False
-
-    def _check_default_secret(self, var_name: str, value: str | None) -> None:
-        if value == "changethis":
-            message = (
-                f'The value of {var_name} is "changethis", '
-                "for security, please change it, at least for deployments."
-            )
-            if self.ENVIRONMENT == "local":
-                warnings.warn(message, stacklevel=1)
-            else:
-                raise ValueError(message)
-
-    @model_validator(mode="after")
-    def _enforce_non_default_secrets(self) -> Self:
-        self._check_default_secret("SECRET_KEY", self.SECRET_KEY)
-        self._check_default_secret("POSTGRES_PASSWORD", self.POSTGRES_PASSWORD)
-        self._check_default_secret(
-            "FIRST_SUPERUSER_PASSWORD", self.FIRST_SUPERUSER_PASSWORD
-        )
-
-        return self
 
 
 settings = Settings()  # type: ignore
