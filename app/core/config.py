ce6d0d2840df1f05d16274a4808047886bcf2325
Mickaël Labarrère <mickael.labarrere@gmail.com>
2024-07-02 14:42:48 +0200

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -16,10 +16,15 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env", env_ignore_empty=True, extra="ignore"
     )
+    PROJECT_NAME : str = "Enkantea"
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
-    # 60 minutes * 24 hours * 8 days = 8 days
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    
+    # Tokens
+    TOKEN_ALGORITHM : str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8 # 60 minutes * 24 hours * 8 days = 8 days
+
+    
     DOMAIN: str = "localhost"
     ENVIRONMENT: Literal["local", "tests", "staging", "production"] = "local"
 
